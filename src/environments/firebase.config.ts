34450b26f140593f837b1c01496c468ff4ceec70
Tymofii <tymofiimustetsov@gmail.com>
2018-03-04 17:41:08 +0200

    My

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,14 @@
+
+
+
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyC1MdmrVkhG1M-9Rav45wdttuo1xKwOoG0",
+    authDomain: "angular-university-4a49d.firebaseapp.com",
+    databaseURL: "https://angular-university-4a49d.firebaseio.com",
+    projectId: "angular-university-4a49d",
+    storageBucket: "angular-university-4a49d.appspot.com",
+    messagingSenderId: "698497001312"
+};
+
+
